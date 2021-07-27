6bbf123cd520600da25153a471dfa8c74c2142a6
Mark Pulido <pulidomark113@gmail.com>
2021-07-27 08:48:07 -0500

    Reusable React Components

--- src/App.js
+++ src/App.js
@@ -50,7 +50,12 @@ const App = () => {
       <div>
         <h1>My Hacker Stories</h1>
 
-        <Search search={searchTerm} onSearch={handleSearch} />
+        <InputWithLabel 
+          id="search"
+          label="Search" 
+          value={searchTerm}
+          onInputChange={handleSearch}
+        />
 
         <hr />
 
@@ -60,18 +65,18 @@ const App = () => {
   )
 };
 
-const Search = ({ search, onSearch }) => {
-  // const { search, onSearch } = props;
-  return(
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" 
-             type="text"
-             value={search} 
-             onChange={onSearch} />
-    </div>
-  );
-};
+const InputWithLabel = ({ id, label, value, onInputChange, type = 'text' }) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
+    <input 
+      id={id} 
+      type={type}
+      value={value} 
+      onChange={onInputChange} 
+    />
+  </>
+);
 
 const List = ({ list }) => (
     <ul>
