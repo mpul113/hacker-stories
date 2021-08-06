cd880b530e7a49982c90b9daa4dffc1e2fe519a5
Mark Pulido <pulidomark113@gmail.com>
2021-08-06 08:28:34 -0500

    Data Fetching w/ React

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import * as React from 'react';
 
-const initialStories = [
-  {
-    title: 'React, ',
-    url: 'https://reactjs.org/, ',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux, ',
-    url: 'https://redux.js.org/, ',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
 
-const getAsyncStories = () => 
-   new Promise((resolve) =>
-     setTimeout(
-       () => resolve({ data: { stories: initialStories } }),
-       2000
-     )
+
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
   );
-  //new Promise((resolve, reject) => setTimeout(reject, 2000));
 
-  const useSemiPersistentState = (key, initialState) => {
-    const [value, setValue] = React.useState(
-      localStorage.getItem(key) || initialState
-    );
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-    React.useEffect(() => {
-      localStorage.setItem(key, value);
-    }, [value, key]);
-
-    return [value, setValue];
-  };
+  return [value, setValue];
+};
 
 
 const ACTION_TYPES = Object.freeze({
@@ -82,6 +56,8 @@ const storiesReducer = (state, action) => {
   }
 };
 
+const API_ENDPOINT = 'http://hn.algolia.com/api/v1/search?query=';
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     'search',
@@ -98,18 +74,18 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: ACTION_TYPES.STORIES_FETCH_INIT});
 
-    getAsyncStories().then(result => {
+    fetch(`${API_ENDPOINT}react`)
+    .then((response) => response.json())
+    .then((result) => {
       dispatchStories({
         type: ACTION_TYPES.STORIES_FETCH_SUCCESS,
-        payload: result.data.stories,
+        payload: result.hits,
       });
     })
     .catch(() => 
-      dispatchStories({ type: ACTION_TYPES.STORIES_FETCH_FAILURE })
-      );
+    dispatchStories({ type: ACTION_TYPES.STORIES_FETCH_FAILURE })
+    );
   }, []);
-
-  //const [stories, setStories] = React.useState(initialStories);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
